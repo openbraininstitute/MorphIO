bae324fa27bacb3d7321d62164086100a7863d6e
Judit Planas <judit.planas@openbraininstitute.org>
2025-11-19 14:25:28 +0100

    Fix typo

--- tests/test_3_h5.py
+++ tests/test_3_h5.py
@@ -58,7 +58,7 @@ def test_wrong_section_type():
 
 def test_v2():
     with pytest.raises(RawDataError,
-                       match='h5v2 is no longer supported, see: https://github.com/OpenBrainInstitute/MorphIO#H5v2'):
+                       match='h5v2 is no longer supported, see: https://github.com/openbraininstitute/MorphIO#H5v2'):
         Morphology(H5V2_PATH / 'Neuron.h5')
 
 
